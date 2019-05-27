9dcd49f546adbf819beda59b12d7a27b3e6bbf51
Nicholas Brinkmann <nicholasbrinkmann@Nicholass-MacBook-Pro.local>
2019-05-27 17:41:15 +0100

    commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+
+export class HomePage {
+  public myDateOfBirth: String;
+
+  ionViewDidEnter() {
+  this.myDateOfBirth = "01-19-1999";
+  }
+
+  constructor(public alertController: AlertController) {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Oooops!',
+      subHeader: 'You silly boy.',
+      message: 'You have downloaded a virus!',
+      buttons: ['I am such a silly boy.']
+    });
+
+    await alert.present();
+  }
+
+  pressMe() {
+    alert("Hi!!!");
+  }
+
+}
+
